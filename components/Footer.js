f9ccdb02c9b0ba136f94a6d539a8dfc6897fc5e8
Madison Ebersole <madison@perygee.com>
2022-08-04 16:30:22 -0400

    Update footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,34 @@
 import React from "react";
 
 const Footer = () => {
-  return <div className="">HElllooooo</div>;
+  return (
+    <div className="mt-12 lg:mt-18 sm:pb-36 sm:py-12 py-6 bg-base-light">
+      <div className="max-w-4xl px-4 mx-auto text-base-dark">
+        <div className="pb-8 mb-2 border-t-2 border-base-highlight"></div>
+        <div className="flex flex-col justify-between lg:flex-row items-center">
+          <p>Built with Next.js, Tailwind and Netify</p>
+          <div className="flex flex-wrap pt-2 sm:space-x-4 space-x-2 font-medium lg:pt-0">
+            <a
+              href="https://www.linkedin.com/in/madison-ebersole/"
+              className={"transition-colors hover:text-base-peach"}
+              target="_blank"
+              rel="noreferrer"
+            >
+              LinkedIn
+            </a>
+            <a
+              href="https://github.com/mae5357/madi-portfolio"
+              className={"transition-colors hover:text-base-peach"}
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
