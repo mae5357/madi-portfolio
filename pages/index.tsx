77b4fdb3b869769b9532dae473bdaa135ce84de6
Madison Ebersole <madison@perygee.com>
2022-08-04 16:29:35 -0400

    Update footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,6 @@ const Home = () => {
           <title>Madison Ebersole</title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
-
         <main className="max-w-4xl mx-auto mt-16 antialiased">
           <About />
         </main>
