a43a11b5016bc3e6cc789d0fdb05f2b0bac89afd
Madison Ebersole <madison@perygee.com>
2022-08-05 13:39:59 -0400

    Delete footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="mt-12 lg:mt-18 sm:pb-36 sm:py-12 py-6 bg-base-light">
       <div className="max-w-4xl px-4 mx-auto text-base-dark">
-        <div className="pb-8 mb-2 border-t-2 border-base-highlight"></div>
+        <div className="pb-8 mb-2 border-t-2"></div>
         <div className="flex flex-col justify-between lg:flex-row items-center">
           <p>Built with Next.js, Tailwind and Netify</p>
           <div className="flex flex-wrap pt-2 sm:space-x-4 space-x-2 font-medium lg:pt-0">
