962ab4175f90a7a5deb7c35d685d23bfd7786442
Madison Ebersole <madison@perygee.com>
2022-08-05 15:26:11 -0400

    Remove container

--- layouts/Layout.js
+++ layouts/Layout.js
@@ -3,7 +3,7 @@ import Footer from "../components/Footer";
 
 export const Layout = ({ children }) => {
   return (
-    <div className="container bg-base-light">
+    <div className="bg-base-light">
       <main>{children}</main>
       <Footer />
     </div>
