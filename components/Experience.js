77a33733beb07ff18b5ba4feb6d3499af24c3242
Madison Ebersole <madison@perygee.com>
2022-08-09 16:27:16 -0400

    Add Experience section

--- components/Experience.js
+++ components/Experience.js
@@ -4,29 +4,65 @@ import profile from "../public/profile.jpg";
 
 const Experience = () => {
   return (
-    <div className="container px-4 mx auto">
-      <div className="lg:space-x-5 lg:flex lg:flex-row item-center lg:-mx-4 flex flex-col-reverse text-center lg:text-left">
-        <div className="lg:px-4 lg:mt-12">
-          <h1 className="text-5xl font-bold text-base-dark">Hi, I'm Madi.</h1>
-          <div className="mt-6 text-lg text-base-dark">
-            <p className="mb-4">
-              I’m an engineer turned data scientist and developer. I like
-              building models, products, and tools that help others.
-            </p>
-          </div>
-        </div>
-        <div className="flex-shrink-0 lg:mt-12 lg:px-4 mb-10">
-          <Image
-            // todo: why so blurry?
-            src={profile}
-            alt="Profile"
-            priority={true}
-            className="rounded-full"
-            width={300}
-            height={300}
-            placeholder="blur"
-          />
-        </div>
+    <div className="flex-1 space-y-8 flex-col container px-10 mx auto text-xl">
+      <p className=" text-right font-bold text-base-peach"> EXPERIENCE </p>
+      <div className=" text-right">
+        <p className="font-bold text-base-dark">Founding Data Scientist @</p>
+        <a
+          href="https://perygee.com/"
+          className="font-light text-base-light-dark transition-colors hover:text-base-peach"
+        >
+          Perygee
+        </a>
+      </div>
+      <div className=" text-right">
+        <p className="font-bold text-base-dark">Data Science Intern @</p>
+        <a
+          href="https://www.tempoautomation.com/"
+          className="font-light text-base-light-dark transition-colors hover:text-base-peach"
+        >
+          Tempo Automations
+        </a>
+      </div>
+      <div className=" text-right">
+        <p className="font-bold text-base-dark">
+          Process Engineering Intern/Co-op @
+        </p>
+        <a
+          href="https://www.qorvo.com/"
+          className="font-light text-base-light-dark transition-colors hover:text-base-peach"
+        >
+          Qorvo
+        </a>
+      </div>
+      <div className=" text-right">
+        <p className="font-bold text-base-dark">Web App developer @</p>
+        <a
+          href="https://www.midcentralenergy.com/"
+          className="font-light text-base-light-dark transition-colors hover:text-base-peach"
+        >
+          Mid Central Energy
+        </a>
+      </div>
+      <div className=" text-right">
+        <p className="font-bold text-base-dark">Bio-Electronic Researcher @</p>
+        <a
+          href="https://sites.psu.edu/ebrahimilab/"
+          className="font-light text-base-light-dark transition-colors hover:text-base-peach"
+        >
+          E-Bio-Electronics Lab
+        </a>
+      </div>
+      <div className=" text-right">
+        <p className="font-bold text-base-dark">
+          Undergraduate Research Assistant @
+        </p>
+        <a
+          href="https://www.internships.giant-grenoble.org/"
+          className="font-light text-base-light-dark transition-colors hover:text-base-peach"
+        >
+          French Alternative Energies and Atomic Energy Commission
+        </a>
       </div>
     </div>
   );
--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="mt-12 lg:mt-18 sm:pb-36 sm:py-12 py-6 bg-base-light">
-      <div className="max-w-4xl px-4 mx-auto text-base-dark">
+      <div className="max-w-4xl px-10 mx-auto text-base-dark">
         <div className="pb-8 mb-2 border-t-2 border-base-highlight"></div>
         <div className="flex flex-col justify-between lg:flex-row items-center">
           <p>Built with Next.js, Tailwind and Netify</p>
