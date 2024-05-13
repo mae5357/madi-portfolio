3d38b09f55cb1f2736e19b518317d5090504e10d
madison <madison.a.ebersole@gmail.com>
2024-05-13 17:07:22 -0400

    init add

---
+++ app/page.js
@@ -0,0 +1,17 @@
+import Header from '../components/Header';
+import About from '../components/About';
+import Projects from '../components/Projects';
+import Footer from '../components/Footer';
+
+const Page = () => {
+  return (
+    <div>
+      <Header />
+      <About />
+      <Projects />
+      <Footer />
+    </div>
+  );
+}
+
+export default Page;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        "base-light": "#f6f7f3",
-        "base-dark": "#0F0D35",
-        "base-light-dark": "#393B63",
-        "base-peach": "#F4BB8A",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
