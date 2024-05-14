51f990b9ea51848972e54b3b9363b5ada2515087
madison <madison.a.ebersole@gmail.com>
2024-05-14 17:27:58 -0400

    working

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,12 +1,14 @@
+'use client'
+
 import React from 'react';
 import './Header.css';
+import Carousel from './Carousel';
 
 function Header() {
   return (
     <header className="Header">
       <h1>Madison Ebersole</h1>
-        <p>NLP Data Engineer</p>
-        <p>transforming data into impactful solutions</p>
+      <Carousel />
     </header>
   );
 }
--- app/components/ProjectList.js
+++ app/components/ProjectList.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Project from './Project';
 
 const projects = [
-  { id: 1, title: 'Project One', description: 'Description of project one.' },
-  { id: 2, title: 'Project Two', description: 'Description of project two.' },
-  { id: 3, title: 'Project Three', description: 'Description of project three.' },
+  // { id: 1, title: 'Project One', description: 'Description of project one.' },
+  // { id: 2, title: 'Project Two', description: 'Description of project two.' },
+  // { id: 3, title: 'Project Three', description: 'Description of project three.' },
 ];
 
 function ProjectList() {
